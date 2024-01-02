55a52ec849893cf74bde9d2a16a8debb6caec7ef
Dimitri Kennedy <dimitrikennedy@gmail.com>
2024-01-02 13:54:22 -0500

    remove unused

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,6 @@ module.exports = {
     "semi": "off",
     "indent": "off",
     "@typescript-eslint/semi": "off",
-    "tailwindcss/no-custom-classname": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": 0,
-    "react/no-unknown-property": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
