2598219b31578e6b13603fcf51a1baa07966f41d
Dimitri Kennedy <dimitrikennedy@gmail.com>
2024-01-02 23:05:00 -0500

    add tests

---
+++ tests/stream.test.ts
@@ -0,0 +1,72 @@
+import Instructor from "@/instructor"
+import { describe, expect, test } from "bun:test"
+import OpenAI from "openai"
+import { z } from "zod"
+
+async function extractUser() {
+  const UserSchema = z.object({
+    age: z.number(),
+    name: z.string().refine(name => name.includes(" "), {
+      message: "Name must contain a space"
+    })
+  })
+
+  type User = Partial<z.infer<typeof UserSchema>>
+
+  const oai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY ?? undefined,
+    organization: process.env.OPENAI_ORG_ID ?? undefined
+  })
+
+  const client = Instructor({
+    client: oai,
+    mode: "FUNCTIONS"
+  })
+
+  const userStream = await client.chat.completions.create({
+    messages: [{ role: "user", content: "Jason Liu is 30 years old" }],
+    model: "gpt-3.5-turbo",
+    response_model: UserSchema,
+    max_retries: 3,
+    stream: true
+  })
+
+  const reader = userStream.readable.getReader()
+  const decoder = new TextDecoder()
+
+  let result: User = {}
+  let done = false
+
+  while (!done) {
+    try {
+      const { value, done: doneReading } = await reader.read()
+      done = doneReading
+
+      if (done) {
+        break
+      }
+
+      const chunkValue = decoder.decode(value)
+      result = JSON.parse(chunkValue)
+
+      expect(result).toHaveProperty("_isValid")
+      expect(result).toHaveProperty("name")
+      expect(result).toHaveProperty("age")
+    } catch (e) {
+      done = true
+      console.log(e)
+      break
+    }
+  }
+
+  return result
+}
+
+describe("StreamFunctionCall", () => {
+  test("Should return extracted name and age based on schema and safe to parse json in stream", async () => {
+    const user = await extractUser()
+
+    expect(user.name).toEqual("Jason Liu")
+    expect(user.age).toEqual(30)
+  })
+})
