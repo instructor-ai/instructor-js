c858bc04d69d17bcce79e9531ae020ebf17b8437
njk112 <naglis98@gmail.com>
2024-01-03 01:57:04 +0000

    initail KG

---
+++ examples/knowledge-graph/index.ts
@@ -0,0 +1,53 @@
+import Instructor from "@/instructor"
+import OpenAI from "openai"
+import { z } from "zod"
+
+const NodeSchema = z.object({
+  id: z.number(),
+  label: z.string(),
+  color: z.string()
+})
+
+const EdgeSchema = z.object({
+  source: z.number(),
+  target: z.number(),
+  label: z.string(),
+  color: z.string().default("black")
+})
+
+const KnowledgeGraphSchema = z.object({
+  nodes: z.array(NodeSchema),
+  edges: z.array(EdgeSchema)
+})
+
+type KnowledgeGraph = z.infer<typeof KnowledgeGraphSchema>
+
+const oai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY ?? undefined,
+  organization: process.env.OPENAI_ORG_ID ?? undefined
+})
+
+const client = Instructor({
+  client: oai,
+  mode: "JSON"
+})
+
+const createGraph = async (input: string): Promise<KnowledgeGraph | undefined> => {
+  const graph: KnowledgeGraph = await client.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Help me understand following by describing as a detailed knowledge graph: ${input}`
+      }
+    ],
+    model: "gpt-3.5-turbo-1106",
+    response_model: KnowledgeGraphSchema,
+    max_retries: 5
+  })
+
+  return graph
+}
+
+const graph = await createGraph("Teach me about quantum mechanics")
+// "{"nodes":[{"id":1,"label":"Quantum Mechanics","color":"blue"},{"id":2,"label":"Wave-Particle Duality","color":"green"},{"id":3,"label":"Quantum Superposition","color":"green"},{"id":4,"label":"Quantum Entanglement","color":"green"},{"id":5,"label":"Quantum Tunneling","color":"green"}],"edges":[{"source":1,"target":2,"label":"Related to","color":"black"},{"source":1,"target":3,"label":"Related to","color":"black"},{"source":1,"target":4,"label":"Related to","color":"black"},{"source":1,"target":5,"label":"Related to","color":"black"}]}"
+console.log({ graph: JSON.stringify(graph) })
